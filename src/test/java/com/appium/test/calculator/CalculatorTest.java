d844dc7b99145adc347e67a9595aa3cf4c8377fc
Hiro Mia <hiro.mia@bracits.com>
2022-05-01 18:22:51 +0600

    Optimize Locator

--- src/test/java/com/appium/test/calculator/CalculatorTest.java
+++ src/test/java/com/appium/test/calculator/CalculatorTest.java
@@ -10,23 +10,23 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClick
 public class CalculatorTest extends BaseCalculatorTest {
     @Test
     public void addSingleDigitShouldSucceed() {
-        WebElement webElement7 = driver.findElement(By.id("com.google.android.calculator:id/digit_7"));
+        WebElement webElement7 = driver.findElement(By.id("digit_7"));
         wait.until(elementToBeClickable(webElement7)).click();
 
-        WebElement webElementInput = driver.findElement(By.id("com.google.android.calculator:id/formula"));
+        WebElement webElementInput = driver.findElement(By.id("formula"));
         webElementInput.isDisplayed();
         String firstInput = webElementInput.getText().trim();
         System.out.println("Input : " + firstInput);
 
-        wait.until(elementToBeClickable(driver.findElement(By.id("com.google.android.calculator:id/op_add")))).click();
+        wait.until(elementToBeClickable(driver.findElement(By.id("op_add")))).click();
 
-        WebElement webElement9 = driver.findElement(By.id("com.google.android.calculator:id/digit_9"));
+        WebElement webElement9 = driver.findElement(By.id("digit_9"));
         wait.until(elementToBeClickable(webElement9)).click();
         String secondInput = webElementInput.getText().trim().split("\\+")[1].trim();
         System.out.println("Input2 : " + secondInput);
 
-        wait.until(elementToBeClickable(driver.findElement(By.id("com.google.android.calculator:id/eq")))).click();
-        String result = driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText().trim();
+        wait.until(elementToBeClickable(driver.findElement(By.id("eq")))).click();
+        String result = driver.findElement(By.id("result_final")).getText().trim();
         System.out.println("Sum : " + result);
 
         Assert.assertEquals((Integer.parseInt(firstInput) + Integer.parseInt(secondInput)), Integer.parseInt(result));
@@ -34,26 +34,26 @@ public class CalculatorTest extends BaseCalculatorTest {
 
     @Test
     public void addDoubleDigitShouldSucceed() {
-        WebElement webElement7 = driver.findElement(By.id("com.google.android.calculator:id/digit_7"));
+        WebElement webElement7 = driver.findElement(By.id("digit_7"));
         wait.until(elementToBeClickable(webElement7)).click();
         wait.until(elementToBeClickable(webElement7)).click();
 
-        WebElement webElementInput = driver.findElement(By.id("com.google.android.calculator:id/formula"));
+        WebElement webElementInput = driver.findElement(By.id("formula"));
         webElementInput.isDisplayed();
         String firstInput = webElementInput.getText().trim();
         System.out.println("Input : " + firstInput);
 
-        wait.until(elementToBeClickable(driver.findElement(By.id("com.google.android.calculator:id/op_add")))).click();
+        wait.until(elementToBeClickable(driver.findElement(By.id("op_add")))).click();
 
-        WebElement webElement9 = driver.findElement(By.id("com.google.android.calculator:id/digit_9"));
+        WebElement webElement9 = driver.findElement(By.id("digit_9"));
         wait.until(elementToBeClickable(webElement9)).click();
         wait.until(elementToBeClickable(webElement9)).click();
 
         String secondInput = webElementInput.getText().trim().split("\\+")[1].trim();
         System.out.println("Input2 : " + secondInput);
 
-        wait.until(elementToBeClickable(driver.findElement(By.id("com.google.android.calculator:id/eq")))).click();
-        String result = driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText().trim();
+        wait.until(elementToBeClickable(driver.findElement(By.id("eq")))).click();
+        String result = driver.findElement(By.id("result_final")).getText().trim();
         System.out.println("Sum : " + result);
 
         Assert.assertEquals((Integer.parseInt(firstInput) + Integer.parseInt(secondInput)), Integer.parseInt(result));
@@ -61,23 +61,23 @@ public class CalculatorTest extends BaseCalculatorTest {
 
     @Test
     public void subtractSingleDigitShouldSucceed() {
-        WebElement webElement7 = driver.findElement(By.id("com.google.android.calculator:id/digit_9"));
+        WebElement webElement7 = driver.findElement(By.id("digit_9"));
         wait.until(elementToBeClickable(webElement7)).click();
 
-        WebElement webElementInput = driver.findElement(By.id("com.google.android.calculator:id/formula"));
+        WebElement webElementInput = driver.findElement(By.id("formula"));
         webElementInput.isDisplayed();
         String firstInput = webElementInput.getText().trim();
         System.out.println("Input : " + firstInput);
 
-        wait.until(elementToBeClickable(driver.findElement(By.id("com.google.android.calculator:id/op_sub")))).click();
+        wait.until(elementToBeClickable(driver.findElement(By.id("op_sub")))).click();
 
-        WebElement webElement9 = driver.findElement(By.id("com.google.android.calculator:id/digit_7"));
+        WebElement webElement9 = driver.findElement(By.id("digit_7"));
         wait.until(elementToBeClickable(webElement9)).click();
 //        String secondInput = webElementInput.getText().trim().split("\\?")[1].trim();
         System.out.println("Input2 : " + webElementInput.getText().trim());
 
-        wait.until(elementToBeClickable(driver.findElement(By.id("com.google.android.calculator:id/eq")))).click();
-        String result = driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText().trim();
+        wait.until(elementToBeClickable(driver.findElement(By.id("eq")))).click();
+        String result = driver.findElement(By.id("result_final")).getText().trim();
         System.out.println("Sum : " + result);
 
         Assert.assertEquals((Integer.parseInt(firstInput) - 7), Integer.parseInt(result));
