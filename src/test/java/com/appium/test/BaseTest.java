9ae4930fb36b8956cbafa02970b497056c3986d9
Hiro Mia <hiro.mia@bracits.com>
2022-05-04 19:57:04 +0600

    Add recording screen

--- src/test/java/com/appium/test/BaseTest.java
+++ src/test/java/com/appium/test/BaseTest.java
@@ -1,18 +1,28 @@
 package com.appium.test;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.screenrecording.CanRecordScreen;
+import org.apache.commons.codec.binary.Base64;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 public abstract class BaseTest extends BaseExtentReportsTest {
-    protected AppiumDriver driver;
+    protected AndroidDriver driver;
     protected WebDriverWait wait;
 
     protected abstract String getAppPackage();
@@ -29,14 +39,40 @@ public abstract class BaseTest extends BaseExtentReportsTest {
         desiredCapabilities.setCapability("appPackage", getAppPackage());
         desiredCapabilities.setCapability("appActivity", getAppActivity());
 
-        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        //Start recording screen
+        ((CanRecordScreen) driver).startRecordingScreen();
 
     }
 
 
     @AfterClass
     public void tearDown() {
+        stopRecording();
         driver.quit();
+    }
+
+    private void stopRecording() {
+        String projectHomeDirectory = System.getProperty("user.dir");
+        String base64String = ((CanRecordScreen) driver).stopRecordingScreen();
+        byte[] data = Base64.decodeBase64(base64String);
+        String destinationPath = projectHomeDirectory + "/build/videos";
+        String videoName = new SimpleDateFormat("yyyy-MM-dd-HH mm-ss-SSS").format(new Date());
+
+        File theDir = new File(destinationPath);
+        if (!theDir.exists()) {
+            theDir.mkdirs();
+        }
+
+        String filePath = destinationPath + "/" + videoName + ".mp4";
+        System.out.println("filePath : " + filePath);
+        Path path = Paths.get(filePath);
+        try {
+            Files.write(path, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
