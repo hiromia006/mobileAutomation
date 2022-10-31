5b5775c614f9daaff90cce9d0bdf24eccbb6c06d
hiromia006 <hiromia006@gmail.com>
2022-11-01 00:41:56 +0600

    Appium with POM and Page Factory

--- src/test/java/com.emiCalculator.pom/BaseTest/EmiCalculatorBaseTest.java
+++ src/test/java/com.emiCalculator.pom/BaseTest/EmiCalculatorBaseTest.java
@@ -2,12 +2,21 @@ package com.emiCalculator.pom.BaseTest;
 
 import com.emiCalculator.pom.util.GeneralUtil;
 import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
 
 public class EmiCalculatorBaseTest {
@@ -25,6 +34,9 @@ public class EmiCalculatorBaseTest {
             driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(GeneralUtil.LOAD_TIME));
 
+            //Start recording screen
+//            driver.startRecordingScreen();
+
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
@@ -33,6 +45,36 @@ public class EmiCalculatorBaseTest {
     @AfterClass
     public void tearDown() {
         driver.quit();
+    }
+
+    private void stopRecording() {
+        String projectHomeDirectory = System.getProperty("user.dir");
+        String base64String = driver.stopRecordingScreen();
+        byte[] data = Base64.decodeBase64(base64String);
+        String destinationPath = projectHomeDirectory + "/build/videos";
+        File theDir = new File(destinationPath);
+        if (!theDir.exists()) {
+            theDir.mkdirs();
+        }
+
+        String filePath = destinationPath + "/" + driver.getDeviceTime().replace(":", "_").replace("+", " ") + ".mp4";
+        System.out.println("filePath : " + filePath);
+        Path path = Paths.get(filePath);
+        try {
+            Files.write(path, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void takeScreenshot() {
+        try {
+            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String currentDir = System.getProperty("user.dir") + "/build/screenshots/";
+            FileUtils.copyFile(scrFile, new File(currentDir + System.currentTimeMillis() + ".png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
